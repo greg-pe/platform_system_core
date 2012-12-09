0b923b4eb6d9f7c4ec01ab16a93041f5f94c1591
Greg_Maahs <projecteliteroms@gmail.com>
2012-12-09 01:45:49 -0500

    Squashed commit  init: Let radio user manipulate service properties  init: Allow device-specific appends to the property service permissio…
    …n structs
    init: Add a mechanism to extend the property service in device configs
    (credit cm)

--- init/property_service.c
+++ init/property_service.c
@@ -50,6 +50,8 @@
 #include "util.h"
 #include "log.h"
 
+
+#include <device_perms.h>
 #define PERSISTENT_PROPERTY_DIR  "/data/property"
 
 static int persistent_properties_loaded = 0;
@@ -58,6 +60,7 @@ static int property_area_inited = 0;
 static int property_set_fd = -1;
 
 /* White list of permissions for setting property services. */
+#ifndef PROPERTY_PERMS
 struct {
     const char *prefix;
     unsigned int uid;
@@ -94,13 +97,21 @@ struct {
     { "persist.security.", AID_SYSTEM,   0 },
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
+    { "net.pdp",          AID_RADIO,    AID_RADIO },
+#ifdef PROPERTY_PERMS_APPEND
+PROPERTY_PERMS_APPEND
+#endif
     { NULL, 0, 0 }
 };
 
+/* Avoid extending this array. Check device_perms.h */
+
+#endif
 /*
  * White list of UID that are allowed to start/stop services.
  * Currently there are no user apps that require.
  */
+#ifndef CONTROL_PERMS
 struct {
     const char *service;
     unsigned int uid;
@@ -108,9 +119,13 @@ struct {
 } control_perms[] = {
     { "dumpstate",AID_SHELL, AID_LOG },
     { "ril-daemon",AID_RADIO, AID_RADIO },
+#ifdef CONTROL_PERMS_APPEND
+CONTROL_PERMS_APPEND
+#endif
      {NULL, 0, 0 }
 };
-
+/* Avoid extending this array. Check device_perms.h */
+#endif
 typedef struct {
     void *data;
     size_t size;
